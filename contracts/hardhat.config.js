303f721d9d47c34a1b87b4c3ad4689ec04f93071
Desmond Chima <desmond@local.dev>
2026-03-06 22:33:01 +0100

    All 5 contracts verified on Base Sepolia

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,4 +1,4 @@
- require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
 module.exports = {
@@ -8,5 +8,18 @@ module.exports = {
       url: process.env.BASE_SEPOLIA_RPC_URL || "",
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     },
+  },
+  etherscan: {
+    apiKey: process.env.BASESCAN_API_KEY,
+    customChains: [
+      {
+        network: "baseSepolia",
+        chainId: 84532,
+        urls: {
+          apiURL: "https://api.etherscan.io/v2/api?chainid=84532",
+          browserURL: "https://sepolia.basescan.org",
+        },
+      },
+    ],
   },
 };
